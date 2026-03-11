92e5db9849942969f0dbff22c11869df5c6b672f
Dnieu <daniel.mafia@sga.pucminas.br>
2026-03-11 09:10:51 -0300

    Add files via upload

---
+++ Trabalho 1/Atividade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, n1, n2, n3, n4;
+            Console.WriteLine("insira um numero entre 1000 e 9999");
+            n = int.Parse(Console.ReadLine());
+            n1 = (n / 1000) % 10;
+            n2 = (n / 100) % 10;
+            n3 = (n / 10) % 10;
+            n4 = n % 10;
+            
+            Console.WriteLine($" seu numero é " + n1.ToString("0"));
+            Console.WriteLine($" seu numero é " + n2.ToString("0"));
+            Console.WriteLine($" seu numero é " + n3.ToString("0"));
+            Console.WriteLine($" seu numero é " + n.ToString("0"));
+
+
+        }
+    }
+}
